3a281d6b66309eeb534249f97397cd3584bc7d09
sergigallardo <60615701+sergigallardo@users.noreply.github.com>
2021-04-12 10:59:08 +0200

    Add files via upload

---
+++ M03. UF3. Activitats1/M03. UF3. Ex1/src/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: 12 abr. 2021
+ *      Author: sergi
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "functions.h"
+
+//PRE: Demana un text al usuari d'un maxim de 100 caracters.
+void main(){
+	missatge();
+}
+//POST: Guarda el text de l'usuari a un arxiu en una ruta especifica.
